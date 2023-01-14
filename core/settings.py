51df2cefba86aee5aa287351ae3532e378af303a
Mahdi Jafari <mahdi100th@gmail.com>
2023-01-14 12:11:46 +0430

    updated postgres in settings

--- core/settings.py
+++ core/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Just after the default DATABASE configuration add this in settings.py
 
-POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_DB =
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
+        'NAME': os.environ.get("POSTGRES_DB"),
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("POSTGRES_HOST"),
